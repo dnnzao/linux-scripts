0ffd1a808f01f83c15fd1bee54d9f49af8216b47
Denio Barbosa Junior <deniojr@gmail.com>
2025-07-18 19:09:47 -0300

    fix bluetooth

--- nixos-flake/hosts/denio.nix
+++ nixos-flake/hosts/denio.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  # Remove the hardware import if you're not ready for it
-  # imports = [ ./hardware-configuration.nix ]; 
+  # Remove hardware import if not ready
+  # imports = [ ./hardware-configuration.nix ];
 
   networking.hostName = "denio-nixos";
   time.timeZone = "America/Sao_Paulo";
@@ -10,29 +10,37 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Proper Bluetooth configuration
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+
   services = {
     greetd.enable = true;
     greetd.settings.default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
     
     pipewire.enable = true;
     pipewire.pulse.enable = true;
-    bluetooth.enable = true;
     docker.enable = true;
   };
 
   users.users.denio = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" "audio" "video" "networkmanager" ];
+    extraGroups = [ "wheel" "docker" "audio" "video" "networkmanager" "bluetooth" ];
     shell = pkgs.zsh;
   };
 
   environment.systemPackages = with pkgs; [
-    # Keep your existing packages
-    # Add essential dependencies for your themes
-    rofi
-    wofi
-    kitty
-    hyprland
+    bluez
+    bluez-tools
+    blueman
+    # Rest of your packages...
   ];
 
   system.stateVersion = "23.05";
